9ce51fb35474b58e9130f25a4645f0165d08d4c5
Chandler Shortlidge <chandlershortlidge@gmail.com>
2025-09-05 10:55:58 +0200

    inital commit

---
+++ .env
@@ -0,0 +1,2 @@
+client_id = "d27240c3a55143ff8bdf61b5bb37ced9"
+client_secret = "86a310cc943544cc820418bcf8836a26"
---
+++ config.py
@@ -0,0 +1,5 @@
+# In your config.py file
+import os
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
